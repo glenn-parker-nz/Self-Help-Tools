d1c503e701b4d82673dfbb42ac337cca474abe95
greenchris <greenglyder@gmail.com>
2011-02-01 15:19:48 +1300

    resolved conflicts

--- mysite/code/StudentCareerPlanner.php
+++ mysite/code/StudentCareerPlanner.php
@@ -1,7 +1,25 @@
 <?php
 
-class StudentCareerPlanner extends StudyTool{
-  
+class StudentCareerPlanner extends StudyTool
+{
+	public static $db = array("InterestPhrase" => 'Text');
+	static $has_one = array('ShowMeImage' => 'Image');
+
+	function getCMSFields() 
+	{
+		// include the standard cms fields
+		SiteTree::disableCMSFieldsExtensions();
+		$fields = parent::getCMSFields();
+		SiteTree::enableCMSFieldsExtensions();
+
+		// add the cms new fields
+		$fields->addFieldToTab("Root.Content.Main", new TextField("InterestPhrase", "Short phrase 
+			that leads to the drop down box for selecting a career"),"Content");
+ 
+		//$this->extend('updateCMSFields', $fields);
+
+		return $fields;
+	} 
 }
 
 class StudentCareerPlanner_Controller extends StudyTool_Controller {
